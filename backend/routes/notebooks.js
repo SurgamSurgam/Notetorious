e884eec4662fcf52ac8114405b929f1af9f5ab19
Deyvi <deyvio@gmail.com>
2019-02-26 10:37:02 -0500

    removed user/user:id from notebooks route - added req.user to query

--- backend/routes/notebooks.js
+++ backend/routes/notebooks.js
@@ -11,10 +11,10 @@ const {
 const { loginRequired } = require("../auth/helpers.js");
 
 /* GET users listing. */
-router.get("/user/:user_id", loginRequired, getAllNotebooks);
-router.get("/user/:user_id/:notebook_id", loginRequired, getSingleNotebook);
-router.post("/user/:user_id",loginRequired, addNotebook);
-router.patch("/user/:user_id/:notebook_id", loginRequired, editNotebook);
-router.delete("/user/:user_id/:notebook_id", loginRequired, deleteNotebook);
+router.get("/", loginRequired, getAllNotebooks);
+router.get("/:notebook_id", loginRequired, getSingleNotebook);
+router.post("/",loginRequired, addNotebook);
+router.patch("/:notebook_id", loginRequired, editNotebook);
+router.delete("/:notebook_id", loginRequired, deleteNotebook);
 
 module.exports = router;
